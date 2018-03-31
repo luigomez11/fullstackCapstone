dbe3e70932ff679754989038ccd0433c4eac9e57
Luis Gomez <luigomez11@gmail.com>
2018-03-31 12:06:53 -0400

    fixed test #6

--- test/test.js
+++ test/test.js
@@ -108,10 +108,6 @@ describe('Calories API resource', function(){
                     expect(res.body.name).to.equal(newFood.name);
                     expect(res.body.calories).to.equal(newFood.calories);
                 })
-                /*.then(function(food){
-                    expect(food.name).to.equal(newFood.name);
-                    expect(food.calories).to.equal(newFood.calories);
-                });*/
         });
     });
 
